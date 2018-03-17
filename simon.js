98b509a2af98e8282eb01d984d09ad58960b9b41
Jose Cuevas <cueramjose@gmail.com>
2018-03-17 10:29:09 -0600

    Agregado funcionalidad de turnos

--- simon.js
+++ simon.js
@@ -1,6 +1,41 @@
 const levels = 15
 
-let keys
+let keys = generateKeys(levels)
+
+function nextLevel (currentLevel) {
+    if (currentLevel == levels) {
+        return alert('Ganaste')
+    }
+
+    alert (`Nivel actual: ${currentLevel + 1}`)
+
+    for (let i = 0; i <= currentLevel; i++) {
+        setTimeout(() => activate(keys[i]), 1000 * (i+1))
+    }
+
+    let i = 0
+    let currentKey = keys[i]
+    window.addEventListener('keydown', onKeydown)
+
+    function onKeydown (ev) {
+        if(ev.keyCode == currentKey) {
+            activate(currentKey, { success: true })
+            i++
+            if (i > currentLevel) {
+                window.removeEventListener('keydown', onKeydown)
+                setTimeout(() => nextLevel(i), 1000)
+            }
+
+            currentKey = keys[i]
+        } else {
+            activate(ev.keyCode, { fail: true})
+            window.removeEventListener('keydown', onKeydown)
+            setTimeout(() => alert('perdiste'), 500)
+        }
+    }
+}
+
+nextLevel(0)
 
 function generateKeys (levels) {
     return new Array(levels).fill(0).map(generateRandomKey)
